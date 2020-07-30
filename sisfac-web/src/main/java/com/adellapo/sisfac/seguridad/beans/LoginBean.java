c6106063c939301792cfb7e099a08a32ffd91bfa
unknown <andres.m.dellaporta@gmail.com>
2020-07-30 14:51:38 -0300

    Configuracion de JSF y creacion de formulario LOGIN + controlador

---
+++ sisfac-web/src/main/java/com/adellapo/sisfac/seguridad/beans/LoginBean.java
@@ -0,0 +1,46 @@
+package com.adellapo.sisfac.seguridad.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "myLoginBean")
+@ViewScoped
+public class LoginBean {
+
+	private String usuario;
+	private String clave;
+	
+	
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	/**
+	 * @return the clave
+	 */
+	public String getClave() {
+		return clave;
+	}
+	/**
+	 * @param clave the clave to set
+	 */
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	/**
+	 * Metodo para validar el usuario
+	 */
+	public void validarUsuario() {
+		
+	}
+	
+}
